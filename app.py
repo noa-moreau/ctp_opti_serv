5069c0f847b185218e6d30ab2a309c8abe1a2e32
Noa Moreau <noa.moreau.etu@univ-lille.fr>
2025-02-28 11:34:02 +0100

    correction de l'app

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', defaults={'my_path': ''})
-@app.route('/<path:my_path>')
+@app.route('/app', defaults={'my_path': ''})
+@app.route('/app/<path:my_path>')
 def index(my_path): 
     return "Hello "+my_path
     
